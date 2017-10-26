648450a2c835e27ccde88ea784c5832a67160b71
Michael Jang <michaeljang86@gmail.com>
2017-10-26 17:29:10 -0400

    Get Current Temperature at Position

--- workshop.js
+++ workshop.js
@@ -7,32 +7,54 @@ function getDistance(pos1, pos2) {
 
 function getIssPosition() {
   return request('http://api.open-notify.org/iss-now.json')
-  .then(
-    function(response) {
-      // Parse as JSON
-      let position = JSON.parse(response)
-      // Return object with lat and lng
-      return {
-        lat: position.iss_position.latitude,
-        lng: position.iss_position.longitude,
-      }
+  .then(response => {
+    // Parse as JSON
+    let position = JSON.parse(response)
+    // Return object with lat and lng
+    return {
+      lat: position.iss_position.latitude,
+      lng: position.iss_position.longitude,
     }
-  )
-  // .then(data => console.log(data))
+  })
 }
 getIssPosition()
+.then(promise => console.log(promise))
+
 
 function getAddressPosition(address) {
+  let URL = 'https://maps.googleapis.com/maps/api/geocode/json?'
+  let API_KEY = 'AIzaSyDMcb59Q8RhTqGuBhRGyY_hBjaUe4S4Rxs'
 
+  return request(`${URL}address=${address}&key=${API_KEY}`)
+  .then(response => {
+    let address = JSON.parse(response)
+    return address.results[0].geometry.location
+  })
 }
+getAddressPosition('122 bevdale rd toronto')
+.then(promise => console.log(promise))
+
 
 function getCurrentTemperatureAtPosition(position) {
-
+  // https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+  let URL = 'https://api.darksky.net/forecast'
+  let API_KEY = '48b6b6b75151fc7513664e14a802c6d1'
+  return request(`${URL}/${API_KEY}/${position.lat},${position.lng}?units=ca`)
+  .then(response => {
+    let data = JSON.parse(response)
+    return data.currently.temperature
+  })
 }
+getAddressPosition('11 florenc st barrie')
+.then(getCurrentTemperatureAtPosition)
+.then(promise => console.log(promise))
+
+
 
 function getCurrentTemperature(address) {
 
 }
+
 
 function getDistanceFromIss(address) {
 
